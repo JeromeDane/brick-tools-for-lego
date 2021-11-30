ba248e8a9250a873ed114d927d420b6e8896e99d
Jerome Dane <jerome.dane@gmail.com>
2021-11-30 15:51:41 -0500

    Add set filtering with improved layout

--- app/components/Themed.tsx
+++ app/components/Themed.tsx
@@ -5,7 +5,7 @@
 
 import * as React from 'react';
 import { FontAwesome } from '@expo/vector-icons';
-import { Text as DefaultText, TouchableOpacity, View as DefaultView } from 'react-native';
+import { Text as DefaultText, TouchableOpacity, View as DefaultView, TextInput as DefaultTextInput } from 'react-native';
 import {Picker as DefaultPicker, PickerProps} from '@react-native-picker/picker'
 
 import Colors from '../constants/Colors';
@@ -40,6 +40,47 @@ export function Text(props: TextProps) {
   return <DefaultText style={[{ color }, style]} {...otherProps} />;
 }
 
+type DefaultTextInputProps = React.ComponentProps<typeof DefaultTextInput>
+interface TextInputProps extends DefaultTextInputProps {
+  label?: string;
+}
+
+export function TextInput(props: TextInputProps ) {
+  const { style, ...otherProps } = props;
+  const backgroundColor = useThemeColor({}, 'background');
+  const color = useThemeColor({}, 'text')
+
+  return <View style={[{
+    flex: 1,
+    flexDirection: 'row',
+    flexGrow: 1,
+    position: 'relative',
+    paddingTop: 8
+  }, props.style]}>
+    {props.label && <Text style={{
+        position: 'absolute',
+        top: 0,
+        left: 6,
+        zIndex: 100,
+        paddingHorizontal: 4,
+        backgroundColor,
+        fontSize: 12
+      }}>
+        {props.label}
+      </Text>
+    }
+    <DefaultTextInput style={[{
+      backgroundColor,
+      borderWidth: 1,
+      borderColor: color,
+      borderRadius: 4,
+      color,
+      padding: 10,
+      flexGrow: 1
+    }, style]} {...otherProps} />
+  </View>
+}
+
 export function View(props: ViewProps) {
   const { style, lightColor, darkColor, ...otherProps } = props;
   const backgroundColor = useThemeColor({ light: lightColor, dark: darkColor }, 'background');
@@ -52,20 +93,49 @@ export function Picker(props: PickerProps) {
   const backgroundColor = useThemeColor({}, 'background');
   const color = useThemeColor({}, 'text');
 
-  return <DefaultPicker
-    style={[{
-      backgroundColor,
-      color,
-      minWidth: 100
-    }, style]} {...otherProps}
-    dropdownIconColor={color} />;
+  return <View style={[{
+    flex: 1,
+    flexDirection: 'row',
+    flexGrow: 1,
+    position: 'relative'
+  }, props.style]}>
+    <View style={{
+      borderColor: color,
+      borderWidth: 1,
+      borderRadius: 4,
+      flexGrow: 1,
+      flexDirection: 'row'
+    }}>
+      {props.label && <Text style={{
+        position: 'absolute',
+        top: -8,
+        left: 4,
+        zIndex: 100,
+        paddingHorizontal: 4,
+        backgroundColor,
+        fontSize: 12
+      }}>
+        {props.label}
+      </Text>}
+      <DefaultPicker
+        style={{
+          color,
+          flexGrow: 1,
+          borderStyle: 'none',
+          backgroundColor,
+          padding: 10
+        }} {...otherProps}
+        dropdownIconColor={color} />
+    </View>
+  </View>
 }
 
 Picker.Item = DefaultPicker.Item
 
 type PaginatorProps = {
   selectedValue: number,
-  onValueChange: Function,
+  onPageChange: Function,
+  onPageSizeChange: Function,
   numItems: number,
   pageSize: number
 }
@@ -74,28 +144,55 @@ export const Paginator = (props: PaginatorProps) => {
   const numPages = Math.ceil(props.numItems/props.pageSize),
         pickerItems = []
   for(let i = 0; i < numPages; i++)
-    pickerItems.push(<Picker.Item label={`Page ${(i + 1)}`} value={i} />)
-  return <View style={{flex: 1, flexDirection: 'row', justifyContent: 'center', alignItems: 'center'}}>
-    <TouchableOpacity
-      style={{height: props.selectedValue == 0 ? 0 : 'auto'}}
-      onPress={() =>
-        props.onValueChange(props.selectedValue == 0 ? 0 : props.selectedValue - 1)
-      }>
-      <Icon name="chevron-left" style={{marginRight: 20}} />
-    </TouchableOpacity>
+    pickerItems.push(
+      <Picker.Item key={i} label={(i + 1).toString()} value={i} />
+    )
+  return <View style={{
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    flexGrow: 1
+  }}>
     <Picker
-      style={{width: 150}}
-      selectedValue={props.selectedValue}
-      onValueChange={props.onValueChange}>
-      {pickerItems}
+      prompt="Show"
+      label="Show"
+      selectedValue={props.pageSize}
+      onValueChange={(num: number) => {
+        props.onPageChange(0)
+        props.onPageSizeChange(num)
+      }}
+      style={{marginRight: props.selectedValue == 0 ? 0 : 20}}>
+      <Picker.Item label="10 per page" value={10} />
+      <Picker.Item label="25 per page" value={25} />
+      <Picker.Item label="50 per page" value={50} />
+      <Picker.Item label="100 per page" value={100} />
     </Picker>
-    <TouchableOpacity
-      style={{height: props.selectedValue == numPages - 1 ? 0 : 'auto'}}
-      onPress={() =>
-        props.onValueChange(props.selectedValue + 1)
-      }>
-      <Icon name="chevron-right" style={{marginLeft: 20}} />
-    </TouchableOpacity>
+    {numPages > 1
+      ? <>
+        <TouchableOpacity
+          style={{height: props.selectedValue == 0 ? 0 : 'auto'}}
+          onPress={() =>
+            props.onPageChange(props.selectedValue == 0 ? 0 : props.selectedValue - 1)
+          }>
+          <Icon name="chevron-left" style={{marginRight: 20}} />
+        </TouchableOpacity>
+        <Picker
+          selectedValue={props.selectedValue}
+          onValueChange={props.onPageChange}
+          label="Current Page">
+          {pickerItems}
+        </Picker>
+        <TouchableOpacity
+          style={{height: props.selectedValue == numPages - 1 ? 0 : 'auto'}}
+          onPress={() =>
+            props.onPageChange(props.selectedValue + 1)
+          }>
+          <Icon name="chevron-right" style={{marginLeft: 20}} />
+        </TouchableOpacity>
+      </>
+      : null
+    }
   </View>
 }
 
--- app/screens/Sets.tsx
+++ app/screens/Sets.tsx
@@ -1,23 +1,38 @@
 import React, { useRef, useState } from 'react'
 import { Image, ScrollView, StyleSheet, ViewPropTypes } from 'react-native'
 import sortBy from 'sort-by'
-import { Paginator, Picker, Text, View } from '../components/Themed'
+import { Paginator, Picker, Text, TextInput, View } from '../components/Themed'
 import { RootTabScreenProps } from '../types'
 import sets from '../data/sets.json'
 
 export default function TabsScreen({ navigation }: RootTabScreenProps<'Sets'>) {
   const [sortField, setSortField] = useState('setNum'),
         [pageSize, setPageSize] = useState(25),
+        [filterBy, setFilterBy] = useState(''),
         [currentPage, setCurrentPage] = useState(0),
-        scrollRef = useRef()
+        scrollRef = useRef(),
+        filteredSets = filterBy
+          ? sets.filter(({name, setNum}) => (setNum + name).toLowerCase().match(filterBy.toLowerCase()))
+          : sets
 
   return (
     <View style={styles.container}>
-      <ScrollView ref={scrollRef} style={{padding: 20, paddingBottom: 100}}>
-        <View style={styles.filterBar}>
-          <Text>Sort by</Text>
+      <ScrollView ref={scrollRef} style={{
+        padding: 20,
+        paddingBottom: 100,
+      }}>
+        <TextInput
+          label="Search Sets"
+          onChangeText={value => {
+            setCurrentPage(0)
+            setFilterBy(value)
+          }} />
+        <View style={{
+          marginBottom: 20,
+          marginTop: 10
+        }}>
           <Picker
-            style={{width: 150}}
+            label="Sort by"
             prompt="Sort by"
             selectedValue={sortField}
             onValueChange={(field: string) => setSortField(field)}>
@@ -30,46 +45,42 @@ export default function TabsScreen({ navigation }: RootTabScreenProps<'Sets'>) {
             <Picker.Item label="Year Released" value="year" />
             <Picker.Item label="Year Released (desc)" value="-year" />
           </Picker>
-          <Text style={{marginLeft: 10}}>Show</Text>
-          <Picker
-            style={{width: 150}}
-            prompt="Show"
-            selectedValue={pageSize}
-            onValueChange={(num: number) => {
-              setCurrentPage(0)
-              setPageSize(num)
-            }}>
-            <Picker.Item label="10 per page" value={10} />
-            <Picker.Item label="25 per page" value={25} />
-            <Picker.Item label="50 per page" value={50} />
-            <Picker.Item label="100 per page" value={100} />
-          </Picker>
         </View>
-        {sets
-          .sort(sortBy(sortField))
-          .slice(currentPage * pageSize, currentPage * pageSize + pageSize)
-          .map(set =>
-            <View key={set.setNum} style={styles.theme}>
-              <Image
-                style={styles.image}
-                source={{uri: `https://images.brickset.com/sets/images/${set.setNum}.jpg`}} />
-              <View>
-                <Text>{set.setNum}</Text>
-                <Text>{set.name}</Text>
-                <Text>{set.numParts.toLocaleString()} parts</Text>
-                <Text>Released in {set.year}</Text>
+        {filteredSets.length
+          ? filteredSets
+            .sort(sortBy(sortField))
+            .slice(currentPage * pageSize, currentPage * pageSize + pageSize)
+            .map(set =>
+              <View key={set.setNum} style={styles.theme}>
+                <Image
+                  style={styles.image}
+                  source={{uri: `https://images.brickset.com/sets/images/${set.setNum}.jpg`}} />
+                <View>
+                  <Text>{set.setNum}</Text>
+                  <Text>{set.name}</Text>
+                  <Text>{set.numParts.toLocaleString()} parts</Text>
+                  <Text>Released in {set.year}</Text>
+                </View>
               </View>
-            </View>
-          )
+            )
+          : <Text style={{textAlign: 'center'}}>
+            No results match your search criteria
+          </Text>
         }
-        <Paginator
-          pageSize={pageSize}
-          numItems={sets.length}
-          onValueChange={val => {
-            scrollRef.current?.scrollTo({y: 0, animated: false})
-            setCurrentPage(val)
-          }}
-          selectedValue={currentPage} />
+        <View style={{height: 20}} />
+        {filteredSets.length
+          ? <Paginator
+              pageSize={pageSize}
+              numItems={filteredSets.length}
+              onPageSizeChange={setPageSize}
+              onPageChange={(val : number) => {
+                scrollRef.current?.scrollTo({y: 0, animated: true})
+                setCurrentPage(val)
+              }}
+              selectedValue={currentPage} />
+          : null
+        }
+        <View style={{height: 50}} />
       </ScrollView>
     </View>
   )
@@ -78,6 +89,11 @@ export default function TabsScreen({ navigation }: RootTabScreenProps<'Sets'>) {
 const styles = StyleSheet.create({
   container: {
     flex: 1
+  },
+  nowrapRow: {
+    flex: 1,
+    flexDirection: 'row-reverse',
+    flexWrap: 'nowrap'
   },
   theme: {
     flex: 1,
@@ -92,9 +108,4 @@ const styles = StyleSheet.create({
     backgroundColor: 'gray',
     marginRight: 10
   },
-  filterBar: {
-    flex: 1,
-    flexDirection: 'row',
-    alignItems: 'center'
-  }
 })
