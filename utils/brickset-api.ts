7649e612fdf5be2db89016833239d40b145cf8d2
Jerome Dane <jerome.dane@gmail.com>
2021-12-28 20:22:42 -0500

    Auto log in with cli brickset API

--- utils/brickset-api.ts
+++ utils/brickset-api.ts
@@ -9,6 +9,7 @@ const api : any = async (method: string, data: Object = {}) => {
       username: process.env.BRICKSET_USERNAME,
       password: process.env.BRICKSET_PASSWORD
     })
+    console.log('Setting user hash:', hash)
     bricksetUserHash = hash
     return api(method, data)
   }
@@ -17,7 +18,10 @@ const api : any = async (method: string, data: Object = {}) => {
     headers: {
       'Content-Type': 'application/x-www-form-urlencoded'
     },
-    body: formUrlEncode(Object.assign({apiKey: process.env.BRICKSET_API_KEY}, data))
+    body: formUrlEncode(Object.assign(
+      {apiKey: process.env.BRICKSET_API_KEY}, data,
+      bricksetUserHash ? {userHash: bricksetUserHash} : {})
+    )
   })
     .then(response => {
       try {
