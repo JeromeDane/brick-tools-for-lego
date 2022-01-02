80ea27ddd0bebf087490369af600b7d4398f157a
Jerome Dane <jerome.dane@gmail.com>
2022-01-02 14:10:21 -0500

    Add elements to data provider

--- src/data/DataProvider.tsx
+++ src/data/DataProvider.tsx
@@ -1,5 +1,5 @@
 import React, {createContext, useState, SetStateAction, Dispatch} from 'react'
-import {BricksetCollection, Parts, Sets} from './types'
+import {BricksetCollection, Elements, Parts, Sets} from './types'
 
 const errorIfSetterNotReady = () => {
   console.error('Setter called before it was ready')
@@ -7,11 +7,13 @@ const errorIfSetterNotReady = () => {
 
 type DataContext = {
   bricksetCollection: BricksetCollection | null;
-  setBricksetCollection: React.Dispatch<React.SetStateAction<BricksetCollection|null>>;
+  setBricksetCollection: Dispatch<SetStateAction<BricksetCollection|null>>;
+  elements: Elements | null,
+  setElements: Dispatch<SetStateAction<Elements|null>>;
   isLoggedInToBrickset: boolean;
   setIsLoggedInToBrickset: Dispatch<SetStateAction<boolean>>;
   parts: Parts | null,
-  setParts: React.Dispatch<React.SetStateAction<Parts|null>>,
+  setParts: Dispatch<SetStateAction<Parts|null>>,
   sets: Sets;
   setSets: Dispatch<SetStateAction<Sets>>;
 }
@@ -19,6 +21,8 @@ type DataContext = {
 export const DataContext = createContext({
   bricksetCollection: null,
   setBricksetCollection: errorIfSetterNotReady,
+  elements: null,
+  setElements: errorIfSetterNotReady,
   isLoggedInToBrickset: false,
   setIsLoggedInToBrickset: errorIfSetterNotReady,
   parts: null,
@@ -29,12 +33,15 @@ export const DataContext = createContext({
 
 const DataProvider = ({children}: {children: JSX.Element[] | JSX.Element}) => {
   const [isLoggedInToBrickset, setIsLoggedInToBrickset] = useState(false),
+        [elements, setElements] = useState<Elements|null>(null),
         [sets, setSets] = useState([] as Sets),
         [bricksetCollection, setBricksetCollection] = useState<BricksetCollection|null>(null),
         [parts, setParts] = useState<Parts|null>(null)
   return <DataContext.Provider value={{
     bricksetCollection,
     setBricksetCollection,
+    elements,
+    setElements,
     isLoggedInToBrickset,
     setIsLoggedInToBrickset,
     parts,
