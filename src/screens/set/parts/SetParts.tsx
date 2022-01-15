252175b87d2c9e58cc8e1612c3923e3585e7006e
Jerome Dane <jerome.dane@gmail.com>
2022-01-15 09:09:32 -0500

    Don't process set parts intil that tab is focused

--- src/screens/set/parts/SetParts.tsx
+++ src/screens/set/parts/SetParts.tsx
@@ -1,64 +1,25 @@
-import React, {useEffect, useMemo, useState} from 'react'
-import {sortBy} from 'sort-by-typescript'
+import React, {useCallback, useEffect, useState} from 'react'
 import {MaterialTopTabScreenProps} from '@react-navigation/material-top-tabs'
-import {ActivityIndicator, ScrollView, View} from 'react-native'
-import {Picker} from '../../../components/Themed'
+import {ScrollView} from 'react-native'
 import {SetTabsParamList} from '../SetScreen'
 import {useSet} from '../../../data/sets'
-import Switch from '../../../components/Switch'
 import LoadingWrapper from '../../../components/LoadingWrapper'
-import {useInventoryParts} from '../../../data/inventory-parts'
-import SetPartPreview from './SetPartPreview'
+import SetPartsList from './SetPartsList'
+import {useFocusEffect} from '@react-navigation/native'
 
 export default function SetPartsScreen({navigation, route: {params: {id}}} : MaterialTopTabScreenProps<SetTabsParamList, 'SetParts'>) {
   const set = useSet(id),
-        inventoryId = (set && set.inventories && set.inventories[0] && set.inventories[0].id) || '',
-        inventoryParts = useInventoryParts(inventoryId),
-        defaultSortOrder = 'element.part.category.name,element.part.subCategory,element.part.width,element.part.length,element.part.height,element.color.sortOrder,name',
-        [sortOrder, setSortOrder] = useState(defaultSortOrder),
-        [showSpareParts, setShowSpareParts] = useState(false),
-        sortedInventortParts = useMemo(
-          () => inventoryParts ? inventoryParts.sort(sortBy.apply(sortBy, sortOrder.split(','))) : null,
-          [inventoryParts]
-        )
+        [hasFocused, setHasFocused] = useState(false)
   useEffect(() => {
     navigation.setOptions({title: `Parts (${set?.numParts.toLocaleString()})`})
   }, [set?.numParts])
+  useFocusEffect(useCallback(() => {
+    if(!hasFocused) setHasFocused(true)
+  }, []))
   return (
     <ScrollView style={{padding: 20}}>
-      <LoadingWrapper>
-        {sortedInventortParts
-          ? <View>
-            <View style={{marginVertical: 10}}>
-              <Picker
-                label="Sort by"
-                selectedValue={sortOrder}
-                onValueChange={value => setSortOrder(value.toString())}>
-                <Picker.Item label="Category, size, and color" value={defaultSortOrder} />
-                <Picker.Item label="Color, category, and size" value={'element.color.sortOrder,element.part.category.name,element.part.subCategory,element.part.width,element.part.length,element.part.height,name'} />
-                <Picker.Item label="Size, category, and color" value={'element.part.width,element.part.length,element.part.height,element.part.category.name,element.part.subCategory,element.color.sortOrder,name'} />
-                <Picker.Item label="Size descending, category, and color" value={'-element.part.width,-element.part.length,-element.part.height,element.part.category.name,element.part.subCategory,element.color.sortOrder,name'} />
-                <Picker.Item label="Size, color, and category" value={'element.part.width,element.part.length,element.part.height,element.color.sortOrder,element.part.category.name,element.part.subCategory,name'} />
-                <Picker.Item label="Size descending, color, and category" value={'-element.part.width,-element.part.length,-element.part.height,element.color.sortOrder,element.part.category.name,element.part.subCategory,name'} />
-              </Picker>
-            </View>
-            <View style={{marginBottom: 20, alignItems: 'flex-end'}}>
-              <Switch
-                label="Show spare parts"
-                onValueChange={setShowSpareParts}
-                value={showSpareParts} />
-            </View>
-            {(showSpareParts ? sortedInventortParts : sortedInventortParts.filter(({isSpare}) => !isSpare))
-              .map((inventoryPart, i: number) =>
-                <SetPartPreview
-                  key={i}
-                  inventortPart={inventoryPart}
-                  onPress={id => { navigation.navigate('Element', {id})}} />
-              )
-            }
-          </View>
-          : <ActivityIndicator color="#aaa" />
-        }
+      <LoadingWrapper loading={!hasFocused}>
+        <SetPartsList set={set} navigation={navigation} />
       </LoadingWrapper>
     </ScrollView>
   )
---
+++ src/screens/set/parts/SetPartsList.tsx
@@ -0,0 +1,64 @@
+import React, {useMemo, useState} from 'react'
+import {sortBy} from 'sort-by-typescript'
+import {ActivityIndicator, View} from 'react-native'
+import {Picker} from '../../../components/Themed'
+import Switch from '../../../components/Switch'
+import LoadingWrapper from '../../../components/LoadingWrapper'
+import {useInventoryParts} from '../../../data/inventory-parts'
+import SetPartPreview from './SetPartPreview'
+import type {Set} from '../../../data/types'
+import type {MaterialTopTabNavigationProp} from '@react-navigation/material-top-tabs/lib/typescript/src/types'
+import type {SetTabsParamList} from '../SetScreen'
+
+type SetPartsListParams = {
+  set?: Set,
+  navigation: MaterialTopTabNavigationProp<SetTabsParamList, 'SetParts'>
+}
+
+export default function SetPartsList({navigation, set}: SetPartsListParams) {
+  const inventoryId = (set && set.inventories && set.inventories[0] && set.inventories[0].id) || '',
+        inventoryParts = useInventoryParts(inventoryId),
+        defaultSortOrder = 'element.part.category.name,element.part.subCategory,element.part.width,element.part.length,element.part.height,element.color.sortOrder,name',
+        [sortOrder, setSortOrder] = useState(defaultSortOrder),
+        [showSpareParts, setShowSpareParts] = useState(false),
+        sortedInventortParts = useMemo(
+          () => inventoryParts ? inventoryParts.sort(sortBy.apply(sortBy, sortOrder.split(','))) : null,
+          [inventoryParts]
+        )
+  return (
+    <LoadingWrapper>
+      {sortedInventortParts
+        ? <View>
+          <View style={{marginVertical: 10}}>
+            <Picker
+              label="Sort by"
+              selectedValue={sortOrder}
+              onValueChange={value => setSortOrder(value.toString())}>
+              <Picker.Item label="Category, size, and color" value={defaultSortOrder} />
+              <Picker.Item label="Color, category, and size" value={'element.color.sortOrder,element.part.category.name,element.part.subCategory,element.part.width,element.part.length,element.part.height,name'} />
+              <Picker.Item label="Size, category, and color" value={'element.part.width,element.part.length,element.part.height,element.part.category.name,element.part.subCategory,element.color.sortOrder,name'} />
+              <Picker.Item label="Size descending, category, and color" value={'-element.part.width,-element.part.length,-element.part.height,element.part.category.name,element.part.subCategory,element.color.sortOrder,name'} />
+              <Picker.Item label="Size, color, and category" value={'element.part.width,element.part.length,element.part.height,element.color.sortOrder,element.part.category.name,element.part.subCategory,name'} />
+              <Picker.Item label="Size descending, color, and category" value={'-element.part.width,-element.part.length,-element.part.height,element.color.sortOrder,element.part.category.name,element.part.subCategory,name'} />
+            </Picker>
+          </View>
+          <View style={{marginBottom: 20, alignItems: 'flex-end'}}>
+            <Switch
+              label="Show spare parts"
+              onValueChange={setShowSpareParts}
+              value={showSpareParts} />
+          </View>
+          {(showSpareParts ? sortedInventortParts : sortedInventortParts.filter(({isSpare}) => !isSpare))
+            .map((inventoryPart, i: number) =>
+              <SetPartPreview
+                key={i}
+                inventortPart={inventoryPart}
+                onPress={id => { navigation.navigate('Element', {id})}} />
+            )
+          }
+        </View>
+        : <ActivityIndicator color="#aaa" />
+      }
+    </LoadingWrapper>
+  )
+}
