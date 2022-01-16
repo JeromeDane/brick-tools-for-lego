e1b410f5b5250b18e25b640749e244ccc04f086b
Jerome Dane <jerome.dane@gmail.com>
2022-01-16 16:09:57 -0500

    Install react-native-paper

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,22 @@ import {StatusBar} from 'expo-status-bar'
 import React from 'react'
 import {SafeAreaProvider} from 'react-native-safe-area-context'
 import DataProvider from './data/DataProvider'
-
+import {DefaultTheme, Provider as PaperProvider} from 'react-native-paper'
 import useCachedResources from './hooks/useCachedResources'
 import useColorScheme from './hooks/useColorScheme'
 import Navigation from './navigation/Navigation'
 
 require('intl')
 require('intl/locale-data/jsonp/en.js')
+
+const theme = {
+  ...DefaultTheme,
+  colors: {
+    ...DefaultTheme.colors,
+    primary: 'tomato',
+    accent: 'yellow'
+  }
+}
 
 export default function App() {
   const isLoadingComplete = useCachedResources()
@@ -19,10 +28,12 @@ export default function App() {
   } else {
     return (
       <SafeAreaProvider>
-        <DataProvider>
-          <Navigation colorScheme={colorScheme} />
-          <StatusBar />
-        </DataProvider>
+        <PaperProvider theme={theme}>
+          <DataProvider>
+            <Navigation colorScheme={colorScheme} />
+            <StatusBar />
+          </DataProvider>
+        </PaperProvider>
       </SafeAreaProvider>
     )
   }
