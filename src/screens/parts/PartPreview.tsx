dd20bdb1f454e5b9a68803c3dc7b7dab81cb4a95
Jerome Dane <jerome.dane@gmail.com>
2022-01-19 19:48:16 -0500

    Use paper cards for part preview

--- src/screens/parts/PartPreview.tsx
+++ src/screens/parts/PartPreview.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
-import {StyleSheet, Image, View} from 'react-native'
-import {Text} from '../../components/Themed'
+import {Image, View} from 'react-native'
+import {Card, Paragraph} from 'react-native-paper'
 import {useGetElementByPartAndColor} from '../../data/elements'
 import {Part} from '../../data/types'
 
@@ -13,34 +13,21 @@ const PartPreview = ({defaultColorId, part}: PartPreviewProps) => {
   const getElementByPartAndColor = useGetElementByPartAndColor(),
         defaultColor = part.colors.find(({id}) => id == defaultColorId) || part.colors[0],
         element = defaultColor && getElementByPartAndColor(part.partNum, defaultColor.id)
-  return <View style={styles.part}>
-    {element &&
-      <Image
-        style={{marginRight: 10, width: 100, height: 100, backgroundColor: 'gray'}}
-        source={{uri: `https://www.lego.com/cdn/product-assets/element.img.lod5photo.192x192/${element.id}.jpg`}} />
-    }
-    <View>
-      <Text>{part.category.name}{part.subCategory ? ', ' + part.subCategory : ''}</Text>
-      <Text>{part.name}</Text>
-      <Text>Part Number: {part.partNum}</Text>
-      <Text>Colors: {part.colors.length}</Text>
-    </View>
-  </View>
+  return <Card style={{marginBottom: 20}}>
+    <Card.Title title={part.name} />
+    <Card.Content style={{flexDirection: 'row'}}>
+      {element &&
+        <Image
+          style={{marginRight: 10, width: 100, height: 100, backgroundColor: 'gray'}}
+          source={{uri: `https://www.lego.com/cdn/product-assets/element.img.lod5photo.192x192/${element.id}.jpg`}} />
+      }
+      <View>
+        <Paragraph>{part.category.name}{part.subCategory ? ', ' + part.subCategory : ''}</Paragraph>
+        <Paragraph>Part Number: {part.partNum}</Paragraph>
+        <Paragraph>Colors: {part.colors.length}</Paragraph>
+      </View>
+    </Card.Content>
+  </Card>
 }
 
 export default PartPreview
-
-const styles = StyleSheet.create({
-  part: {
-    flex: 1,
-    flexDirection: 'row',
-    textAlign: 'left',
-    paddingVertical: 10,
-    paddingRight: 20,
-    flexGrow: 1
-  },
-  image: {
-    backgroundColor: 'gray',
-    marginRight: 10
-  }
-})
