77f54ff7710025cd07c27caf10e6e2d25b75b421
Jerome Dane <jerome.dane@gmail.com>
2022-01-19 19:50:57 -0500

    Use paper cards for set part preview

--- src/screens/set/parts/SetPartPreview.tsx
+++ src/screens/set/parts/SetPartPreview.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
-import {Image, View, TouchableOpacity} from 'react-native'
+import {Image, View} from 'react-native'
+import {Card} from 'react-native-paper'
 import {Text} from '../../../components/Themed'
 import {InventoryPart} from '../../../data/types'
 
@@ -9,17 +10,19 @@ type SetPartPreviewProps = {
 }
 
 export default function SetPartPreview({onPress, inventortPart: {part, color, isSpare, quantity, element}}: SetPartPreviewProps) {
-  return <TouchableOpacity style={{flex: 1, flexDirection: 'row', flexGrow: 1, marginBottom: 10}}
+  return <Card style={{marginBottom: 20}}
     onPress={() => onPress(element.id)}>
-    <Image
-      style={{marginRight: 10, width: 100, height: 100, backgroundColor: 'gray'}}
-      source={{uri: `https://www.lego.com/cdn/product-assets/element.img.lod5photo.192x192/${element.id}.jpg`}} />
-    <View>
-      <Text>Part: {part.partNum} Element: {element.id}</Text>
-      <Text>{part.category.name}{part.subCategory ? ', ' + part.subCategory : ''}</Text>
-      <Text>{part.name}</Text>
-      <Text>{color.name} ({color.id})</Text>
-      <Text>Qty: {quantity}{isSpare ? ' (spare part)' : ''}</Text>
-    </View>
-  </TouchableOpacity>
+    <Card.Title title={part.name} />
+    <Card.Content style={{flexDirection: 'row'}}>
+      <Image
+        style={{marginRight: 10, width: 100, height: 100, backgroundColor: 'gray'}}
+        source={{uri: `https://www.lego.com/cdn/product-assets/element.img.lod5photo.192x192/${element.id}.jpg`}} />
+      <View>
+        <Text>Part: {part.partNum} Element: {element.id}</Text>
+        <Text>{part.category.name}{part.subCategory ? ', ' + part.subCategory : ''}</Text>
+        <Text>{color.name} ({color.id})</Text>
+        <Text>Qty: {quantity}{isSpare ? ' (spare part)' : ''}</Text>
+      </View>
+    </Card.Content>
+  </Card>
 }
