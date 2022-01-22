00925aa123fb5b5ec1a7eff5c929aa9bfeb2e24b
Jerome Dane <jerome.dane@gmail.com>
2022-01-22 09:57:48 -0500

    Use react-native-paper on settings sreen

--- src/screens/SettingsScreen.tsx
+++ src/screens/SettingsScreen.tsx
@@ -1,6 +1,6 @@
 import React, {useState} from 'react'
-import {StyleSheet, Button, ScrollView} from 'react-native'
-import {Text, TextInput, View} from '../components/Themed'
+import {ScrollView, View} from 'react-native'
+import {Button, Card, Paragraph, TextInput} from 'react-native-paper'
 import Spinner from '../components/Spinner'
 import {useBricksetCollection, useFetchBricksetCollection, useLogin, useLogOut} from '../api/brickset'
 import {useIsLoggedInToBrickset} from '../api/brickset'
@@ -19,63 +19,60 @@ export default function SettingsScreen() {
   return (
     <ScrollView style={{padding: 20}}>
       <Spinner visible={Boolean(loadingMessage)} textContent={loadingMessage} />
-      <Text style={styles.heading}>Brickset</Text>
       <RequireAPISupport>
-        {isLoggedIn
-          ? <View>
-            <Text style={{marginBottom: 20}}>
-              Found {Object.keys(collection || {}).length.toLocaleString()} sets you have tracked on Brickset. Any changes
-              you make in this app will be automatically synced back to Brickset as long as you remain logged in.
-            </Text>
-            <View style={{marginBottom: 20}}>
-              <Button
-                title="Re-sync my collection from Brickset" onPress={() => {
+        <Card>
+          <Card.Title title="Brickset" />
+          <Card.Content>
+            {isLoggedIn
+              ? <View>
+                <Paragraph style={{marginBottom: 20}}>
+                    Found {Object.keys(collection || {}).length.toLocaleString()} sets you have tracked on Brickset. Any changes
+                    you make in this app will be automatically synced back to Brickset as long as you remain logged in.
+                </Paragraph>
+                <Button icon="sync" mode="contained" style={{marginBottom: 20}} onPress={() => {
                   setLoadingMessage('Syncing collection from Brickset ...')
                   fetchBricksetCollection()
                     .then(() => setLoadingMessage(''))
                     .catch(() => setLoadingMessage(''))
-                }}/>
-            </View>
-            <Button title="Log out from Brickset" onPress={() => {
-              setLoadingMessage('Logging out from Brickset ...')
-              logOut()
-                .then(() => setLoadingMessage(''))
-                .catch(() => setLoadingMessage(''))
-            }} />
-          </View>
-          : <View>
-            <Text style={{marginBottom: 10}}>
-              Log into your{' '}
-              <TextLink url="https://www.brickset.com">Brickset.com</TextLink>{' '}
-              account to access your personal data. If you don&apos;t already have one, you&apos;ll need to{' '}
-              <TextLink url="https://brickset.com/signup">create an account</TextLink>.
-            </Text>
-            <TextInput label="username" onChangeText={setUsername} style={{marginBottom: 10}} />
-            <TextInput label="password" secureTextEntry={true} onChangeText={setPassword} style={{marginBottom: 10}} />
-            <Button title="Login" onPress={() => {
-              setLoadingMessage('Logging in to Brickset ...')
-              login(username, password)
-                .then(() => {
-                  setLoadingMessage('Importing collection from Brickset ...')
-                  fetchBricksetCollection()
+                }}>
+                  Re-sync my collection
+                </Button>
+                <Button icon="logout" mode="contained" onPress={() => {
+                  setLoadingMessage('Logging out from Brickset ...')
+                  logOut()
                     .then(() => setLoadingMessage(''))
                     .catch(() => setLoadingMessage(''))
-                })
-                .catch(() => setLoadingMessage(''))
-            }}/>
-          </View>
-        }
+                }}>
+                  Log out
+                </Button>
+              </View>
+              : <View>
+                <Paragraph style={{marginBottom: 10}}>
+                  Log into your{' '}
+                  <TextLink url="https://www.brickset.com">Brickset.com</TextLink>{' '}
+                  account to access your personal data. If you don&apos;t already have one, you&apos;ll need to{' '}
+                  <TextLink url="https://brickset.com/signup">create an account</TextLink>.
+                </Paragraph>
+                <TextInput mode="outlined" label="Username" onChangeText={setUsername} style={{marginBottom: 10}} autoComplete={true} />
+                <TextInput mode="outlined" label="Password" secureTextEntry={true} onChangeText={setPassword} style={{marginBottom: 10}} autoComplete={false} />
+                <Button mode="contained" icon="login" onPress={() => {
+                  setLoadingMessage('Logging in to Brickset ...')
+                  login(username, password)
+                    .then(() => {
+                      setLoadingMessage('Importing collection from Brickset ...')
+                      fetchBricksetCollection()
+                        .then(() => setLoadingMessage(''))
+                        .catch(() => setLoadingMessage(''))
+                    })
+                    .catch(() => setLoadingMessage(''))
+                }}>
+                  Login
+                </Button>
+              </View>
+            }
+          </Card.Content>
+        </Card>
       </RequireAPISupport>
     </ScrollView>
   )
 }
-
-const styles = StyleSheet.create({
-  linkText: {
-    color: '#2e78b7'
-  },
-  heading: {
-    marginBottom: 20,
-    fontWeight: 'bold'
-  }
-})
