1322f3a21f9d6c907e013f38ae25151d083f7eeb
Jerome Dane <jerome.dane@gmail.com>
2022-01-22 10:09:56 -0500

    Use react-native-paper on about sreen

--- src/screens/AboutScreen.tsx
+++ src/screens/AboutScreen.tsx
@@ -1,54 +1,64 @@
 import * as React from 'react'
-import {StyleSheet, Linking, Button, ScrollView} from 'react-native'
-import {View} from '../components/Themed'
+import {StyleSheet, Linking, ScrollView} from 'react-native'
+import {Button, Card, Paragraph} from 'react-native-paper'
 import TextLink from '../components/TextLink'
 import {version} from '../../package.json'
-import { Paragraph } from 'react-native-paper'
 
 export default function AboutScreen() {
   return (
     <ScrollView style={styles.container}>
-      <Paragraph>
-        This project is free, open source, and maintained by volunteers out of a love of all things LEGO®.{' '}
-        If you&apos;d like to get involved in any way, check out this project&apos;s{' '}
-        <TextLink url="https://github.com/JeromeDane/brick-tools-for-lego">
-          GitHub page
-        </TextLink>.
-      </Paragraph>
-      <TextLink style={styles.paragraph} url="https://github.com/JeromeDane/brick-tools-for-lego/issues?q=is%3Aopen+is%3Aissue+label%3Aenhancement">
-        Planned features
-      </TextLink>
-      <TextLink style={styles.paragraph} url="https://github.com/JeromeDane/brick-tools-for-lego/issues?q=is%3Aopen+is%3Aissue+label%3Abug">
-        Known bugs
-      </TextLink>
-      <TextLink style={styles.paragraph} url="https://twitter.com/BrickTools4LEGO">
-        @BrickTools4LEGO on Twitter
-      </TextLink>
-      <TextLink style={styles.paragraph} url="https://www.reddit.com/r/BrickToolsForLEGO/">
-        Reddit community: r/BrickToolsForLEGO/
-      </TextLink>
-      <Paragraph>
-        Show your ❤️ for this project:
-      </Paragraph>
-      <View style={styles.paragraph}>
-        <Button
-          title="Make a donation"
-          onPress={() => Linking.openURL('https://www.paypal.com/donate?hosted_button_id=LWSCPL34NZ6LJ')} />
-      </View>
-      <Paragraph>
-        Author:{' '}
-        <TextLink url="https://www.linkedin.com/in/JeromeDane/">
-          Jerome Dane
-        </TextLink>
-      </Paragraph>
-      <Paragraph>Version {version}</Paragraph>
-      <Paragraph>
-        Powered by data from{' '}
-        <TextLink url="https://www.brickset.com/">Brickset.com</TextLink>
-        {' '}and{' '}
-        <TextLink url="https://www.rebrickable.com/">Rebrickable.com</TextLink>
-      </Paragraph>
-      <Paragraph>LEGO® is a trademark of the LEGO Group of companies which does not sponsor, authorize or endorse this project.</Paragraph>
+      <Card style={{marginBottom: 20}}>
+        <Card.Title title="Project Overview" />
+        <Card.Content>
+          <Paragraph>
+            This project is free, open source, and maintained by volunteers out of a love of all things LEGO®.{' '}
+            If you&apos;d like to get involved in any way, check out this project&apos;s{' '}
+            <TextLink url="https://github.com/JeromeDane/brick-tools-for-lego">
+              GitHub page
+            </TextLink>
+            . Show your ❤️ for this project:
+          </Paragraph>
+          <Button style={{marginTop: 10}} mode="contained" icon="gift-outline"
+            onPress={() => Linking.openURL('https://www.paypal.com/donate?hosted_button_id=LWSCPL34NZ6LJ')}>
+              Make a donation
+          </Button>
+          <Paragraph style={{marginTop: 20}}>
+            Version {version}
+          </Paragraph>
+          <Paragraph>
+            Author:{' '}
+            <TextLink url="https://www.linkedin.com/in/JeromeDane/">
+              Jerome Dane
+            </TextLink>
+          </Paragraph>
+          <Paragraph style={{marginTop: 20}}>
+            Powered by data from{' '}
+            <TextLink url="https://www.brickset.com/">Brickset.com</TextLink>
+            {' '}and{' '}
+            <TextLink url="https://www.rebrickable.com/">Rebrickable.com</TextLink>
+          </Paragraph>
+          <Paragraph style={{marginTop: 20, fontStyle: 'italic'}}>
+            LEGO® is a trademark of the LEGO Group of companies which does not sponsor, authorize or endorse this project.
+          </Paragraph>
+        </Card.Content>
+      </Card>
+      <Card>
+        <Card.Title title="Project Links" />
+        <Card.Content>
+          <Button icon="chart-gantt" onPress={() => Linking.openURL('https://github.com/JeromeDane/brick-tools-for-lego/issues?q=is%3Aopen+is%3Aissue+label%3Aenhancement')}>
+            Planned features
+          </Button>
+          <Button icon="bug" onPress={() => Linking.openURL('https://github.com/JeromeDane/brick-tools-for-lego/issues?q=is%3Aopen+is%3Aissue+label%3Abug')}>
+            Known bugs
+          </Button>
+          <Button icon="twitter" onPress={() => Linking.openURL('https://twitter.com/BrickTools4LEGO')}>
+            @BrickTools4LEGO on Twitter
+          </Button>
+          <Button icon="reddit" onPress={() => Linking.openURL('https://www.reddit.com/r/BrickToolsForLEGO/')}>
+            Reddit community: r/BrickToolsForLEGO/
+          </Button>
+        </Card.Content>
+      </Card>
     </ScrollView>
   )
 }
