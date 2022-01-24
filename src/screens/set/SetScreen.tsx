b19fc48d37789e67f4674b159be9b1335bb643a0
Jerome Dane <jerome.dane@gmail.com>
2022-01-23 20:24:55 -0500

    Fix sets tabs not refreshing on new set

--- src/screens/set/SetScreen.tsx
+++ src/screens/set/SetScreen.tsx
@@ -5,6 +5,7 @@ import SetPartsScreen from './parts/SetParts'
 import SetInstructionsScreen from './SetInstructions'
 import {useSet} from '../../data/sets'
 import {RootStackParamList} from '../../navigation/types'
+import {useIsFocused} from '@react-navigation/native'
 
 export type SetTabsParamList = {
   SetDetails: {id: string};
@@ -15,27 +16,30 @@ export type SetTabsParamList = {
 const Tab = createMaterialTopTabNavigator()
 
 const SetTabs = ({navigation: {setOptions}, route: {params: {id}}}: MaterialTopTabScreenProps<RootStackParamList, 'Set'>) => {
-  const set = useSet(id)
+  const set = useSet(id),
+        isFocused = useIsFocused()
   useEffect(() => {
     setOptions({title: set?.setNum.replace(/-.*$/, '') + ' ' + set?.name})
   }, [set])
-  return <Tab.Navigator initialRouteName="SetDetails">
-    <Tab.Screen
-      name="SetDetails"
-      initialParams={{id}}
-      options={{title: 'Details'}}
-      component={SetDetailsScreen} />
-    <Tab.Screen
-      name="SetParts"
-      initialParams={{id}}
-      options={{title: 'Parts'}}
-      component={SetPartsScreen} />
-    <Tab.Screen
-      name="SetInstructions"
-      initialParams={{id}}
-      options={{title: 'Instructions'}}
-      component={SetInstructionsScreen} />
-  </Tab.Navigator>
+  return isFocused
+    ? <Tab.Navigator initialRouteName="SetDetails">
+      <Tab.Screen
+        name="SetDetails"
+        initialParams={{id}}
+        options={{title: 'Details'}}
+        component={SetDetailsScreen} />
+      <Tab.Screen
+        name="SetParts"
+        initialParams={{id}}
+        options={{title: 'Parts'}}
+        component={SetPartsScreen} />
+      <Tab.Screen
+        name="SetInstructions"
+        initialParams={{id}}
+        options={{title: 'Instructions'}}
+        component={SetInstructionsScreen} />
+    </Tab.Navigator>
+    : null
 }
 
 export default SetTabs
